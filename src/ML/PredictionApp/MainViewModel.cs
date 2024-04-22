89f458e781987f602cd6ce6ec511b46c1e77f27c
Arkarin0 <snaut@hotmail.de>
2024-04-22 22:55:34 +0200

    added ml Learner. added a Preditionap which uses the learner data to predict the Material.

---
+++ src/ML/PredictionApp/MainViewModel.cs
@@ -0,0 +1,57 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Learner;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredictionApp
+{
+    public partial class MainViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        int _UVValue= 5_000;
+
+        [ObservableProperty]
+        float _surfaceTexture=10f;
+
+        [ObservableProperty]
+        ushort _magneticApmlitude=35_000;
+
+        [ObservableProperty]
+        float _magneticPhase=180f;
+
+        [ObservableProperty]
+        string _result;
+
+        public MainViewModel()
+        {
+            
+        }
+
+        [RelayCommand]
+        private void Predict()
+        {
+            //Load sample data
+            var sampleData = new ClassificationAppModel.ModelInput()
+            {
+                Col1 = this.SurfaceTexture,
+                Col2 = this.UVValue,
+                Col3 = this.MagneticPhase,
+                Col4 = this.MagneticApmlitude,
+            };
+
+            //Load model and predict output
+            var result = ClassificationAppModel.Predict(sampleData);
+            var labels= ClassificationAppModel.GetSortedScoresWithLabels(result);
+
+            this.Result =
+                $"winner:\t{result.PredictedLabel}\n\n" + 
+                string.Join("\n", labels.Where(v=> v.Value>0.0001).Select((v) => $"{v.Key,-12}\t: {v.Value*100,5:00.00}%"));
+
+        }
+    }
+}
---
+++ src/ML/learner/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Learner;
+
+Console.WriteLine("Hello, We will now start learning the data.");
+
+string resultFile = Path.Join(Directory.GetCurrentDirectory(), "mlModel.zip");
+ClassificationAppModel.Train(resultFile);
+Console.WriteLine($"training finished. Model saved to '{resultFile}'.");
